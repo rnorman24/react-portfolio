bc3004cc1eab8a0022ab6b9222432997bda329dc
unknown <nray24@gmail.com>
2018-09-14 11:23:24 -0600

    css refactor

--- src/components/About.js
+++ src/components/About.js
@@ -1,40 +1,77 @@
 import React from "react";
 
-
 const About = () => (
   <header>
     <div className="header-wrapper">
-  <h1>The Experimental Project Lab</h1><h1>of Raymond Norman</h1>
-  <img src={require("./images/rn-150-image.png")} alt="Raymond Norman" />
-  <div className="demo-list">
-      <ul>
-        <li><a href="https://youtube.com/layoutland">Layout Land videos</a></li>
-        <li><a href="http://layout.land">Layout Land website</a></li> 
-        <li><a href="http://jensimmons.com/post/feb-27-2017/learn-css-grid">Learn how to use CSS Grid</a></li>
-        <li><a href="http://jensimmons.com/presentation/real-art-direction-web">Revolutionize Your Page: Real Art Direction on the Web</a></li>    
-        <li><a href="http://jensimmons.com/presentation/progressing-our-layouts">Progressing Our Layouts</a></li>
-        <li><a href="http://jensimmons.com/presentation/modern-layouts-getting-out-our-ruts">Modern Layouts: Getting Out of Our Ruts</a></li> 
-      <li><a href="http://jensimmons.com">jensimmons.com</a></li>
-
-      <li><a href="../2017/workshop">Workshop Examples</a></li>
-      </ul>
-  </div>
-  <p className="follow-links">Follow <a href="#">@raymondnorman</a> on Twitter for more as it happens.<br/>Contact me at <a href="#">email</a>.</p>
-  <div className="workshop-link">
-    <ul>           
-      <li><a href="https://developer.mozilla.org/docs/Tools/Page_Inspector/How_to/Examine_grid_layouts">Firefox Grid Inspector</a></li>
-      <li><a href="https://developer.mozilla.org/en-US/docs/Tools/Page_Inspector/How_to/Edit_CSS_shapes">Firefox Shape Path Editor</a></li>
-      <li><a href="http://nightly.mozilla.org">Firefox Nightly</a></li>
-    </ul>
-  </div>
-</div>
-</header>
-
+      <h1>The Experimental Project Lab</h1>
+      <h1>of Raymond Norman</h1>
+      <img src={require("./images/rn-150-image.png")} alt="Raymond Norman" />
+      <div className="demo-list">
+        <ul>
+          <li>
+            <a href="https://youtube.com/layoutland">Layout Land videos</a>
+          </li>
+          <li>
+            <a href="http://layout.land">Layout Land website</a>
+          </li>
+          <li>
+            <a href="http://jensimmons.com/post/feb-27-2017/learn-css-grid">
+              Learn how to use CSS Grid
+            </a>
+          </li>
+          <li>
+            <a href="http://jensimmons.com/presentation/real-art-direction-web">
+              Revolutionize Your Page: Real Art Direction on the Web
+            </a>
+          </li>
+          <li>
+            <a href="http://jensimmons.com/presentation/progressing-our-layouts">
+              Progressing Our Layouts
+            </a>
+          </li>
+          <li>
+            <a href="http://jensimmons.com/presentation/modern-layouts-getting-out-our-ruts">
+              Modern Layouts: Getting Out of Our Ruts
+            </a>
+          </li>
+          <li>
+            <a href="http://jensimmons.com">jensimmons.com</a>
+          </li>
+          <li>
+            <a href="../2017/workshop">Workshop Examples</a>
+          </li>
+        </ul>
+      </div>
+      <p className="follow-links">
+        Follow <a href="#">@raymondnorman</a> on Twitter for more as it happens.
+        <br />
+        Contact me at <a href="#">email</a>.
+      </p>
+      <div className="workshop-link">
+        <ul>
+          <li>
+            <a href="https://developer.mozilla.org/docs/Tools/Page_Inspector/How_to/Examine_grid_layouts">
+              Firefox Grid Inspector
+            </a>
+          </li>
+          <li>
+            <a href="https://developer.mozilla.org/en-US/docs/Tools/Page_Inspector/How_to/Edit_CSS_shapes">
+              Firefox Shape Path Editor
+            </a>
+          </li>
+          <li>
+            <a href="http://nightly.mozilla.org">Firefox Nightly</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
 );
 
 export default About;
 
-{ /*  <header
+{
+  /*  <header
   className="masthead text-center text-white"
   style={{
     backgroundImage: "url(" + imgUrl + ")",
@@ -55,4 +92,5 @@ export default About;
   <a className="btn btn-primary btn-xl" href="#about">
     Find Out More
   </a>
-</header> */ }
+</header> */
+}
