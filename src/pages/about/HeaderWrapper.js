8560133c7978fa9320a0c2a196167f7c9960de0b
unknown <nray24@gmail.com>
2018-09-17 15:57:07 -0600

    css refactor

--- src/pages/about/HeaderWrapper.js
+++ src/pages/about/HeaderWrapper.js
@@ -16,16 +16,37 @@ const HeaderWrapper = styled("div")(
   }
 );
 
-const h1 = css({
-  fontSize: "calc(1.3rem + 1.8vw)",
-  margin: 0,
-  lineHeight: "1.2rem"
-});
+const H1nth1 = styled('h1:nth-child(1)')(
+  {
+    gridColumn: '1 / 4',
+    textAlign: 'right',
+    fontSize: "calc(1.3rem + 1.8vw)",
+    margin: 0,
+    lineHeight: "1.2rem"
+  }
+)
+
+const H1nth2 = styled('h1:nth-child(2)')(
+    props => ({
+      color: props.theme.red
+    }),
+    {
+      writingMode: 'vertical-lr',
+      gridColumn: '3 / 4',
+      gridRow: '2 / 5',
+      justifySelf: 'end',
+      marginTop: -10,
+      marginRight: -10,
+      fontSize: "calc(1.3rem + 1.8vw)",
+      lineHeight: "1.2rem"
+    }
+);
+
 
 const HeaderWrapper = () => (
   <HeaderWrapper>
-      <h1 className={h1}>The Experimental Project Lab</h1>
-      <h1 className={h1}>of Raymond Norman</h1>
+      <H1nth1>The Experimental Project Lab</H1nth1>
+      <H1nth2>of Raymond Norman</H1nth2>
       <img src={require("../images/rn-150-image.png")} alt="Raymond Norman" />
       <div className="demo-list">
         <ul>
