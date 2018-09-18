a833c2ef7c528ba6c34e056fbe6d0ef1f4ac3a8a
unknown <nray24@gmail.com>
2018-09-18 09:13:51 -0600

    css refactor

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,16 @@ import React from "react";
 import styled from "react-emotion";
 import HeaderWrapper from "./about/HeaderWrapper";
 
+const breakpoints = [500, 700, 820, 1000, 1300, 1800];
+
+const mqMax = breakpoints.map(
+  bp => `@media (max-width: ${bp}px)`
+);
+
+const mqMin = breakpoints.map(
+  bp => `@media (min-width: ${bp}px)`
+);
+
 const Header = styled("header")(
   props => ({
     background: props.theme.bgColor
@@ -10,6 +20,19 @@ const Header = styled("header")(
     display: 'flex',
     margin: 'auto',
     padding: '1rem 5vw 12rem',
+    [mqMax[2]]: {
+      paddingTop: '5rem',
+      paddingBottom: '5rem'
+    },
+    [mqMin[3]]: {
+      paddingBottom: '15rem'
+    },
+    [mqMin[4]]: {
+      paddingBottom: '25rem'
+    },
+    [mqMin[5]]: {
+      paddingBottom: '30rem'
+    },
     minHeight: "100vh"    
   }
 );
