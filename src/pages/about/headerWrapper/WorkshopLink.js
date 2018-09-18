7c001d5fd01c0c1ac43c2081351c146509ee93fb
unknown <nray24@gmail.com>
2018-09-18 11:03:58 -0600

    css media queries build

--- src/pages/about/headerWrapper/WorkshopLink.js
+++ src/pages/about/headerWrapper/WorkshopLink.js
@@ -12,12 +12,17 @@ const WorkshopStyle = styled("div")({
   gridRow: "3",
   fontWeight: "700",
   alignSelf: "end",
+  borderTop: '5px solid #3c3b37',
+  borderBottom: '5px solid #3c3b37',
   [mqMax[2]]: {
     alignSelf: 'start',
     marginLeft: '1rem'
   },
-  borderTop: '5px solid #3c3b37',
-  borderBottom: '5px solid #3c3b37'
+  [mqMax[0]]: {
+    gridColumn: '1 / -1',
+    gridRow: '5',
+    marginLeft: 0
+  }
 });
 
 const WorkshopUl = styled("ul")({
