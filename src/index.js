c94fd2c0a4f5901ff42930a1e3f4118ef7ee6b7d
unknown <nray24@gmail.com>
2018-09-21 14:20:35 -0600

    'commit'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 /* import 'bootstrap/dist/css/bootstrap.css'; */
-/* import './styles.css'; */
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
