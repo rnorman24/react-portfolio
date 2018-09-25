d3bd534994c8448c88969e710c54d2d625b3716c
unknown <nray24@gmail.com>
2018-09-25 14:33:49 -0600

    refactor react

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,36 @@
 import React from "react";
 import styled from "react-emotion";
+
 import HeaderWrapper from "./home/HeaderWrapper";
 
 const breakpoints = [500, 700, 820, 1000, 1300, 1800];
 
-const mqMax = breakpoints.map(
-  bp => `@media (max-width: ${bp}px)`
-);
+const mqMax = breakpoints.map(bp => `@media (max-width: ${bp}px)`);
 
-const mqMin = breakpoints.map(
-  bp => `@media (min-width: ${bp}px)`
-);
+const mqMin = breakpoints.map(bp => `@media (min-width: ${bp}px)`);
 
 const Header = styled("header")(
   props => ({
     background: props.theme.bgColor
   }),
   {
-    display: 'flex',
-    margin: 'auto',
-    padding: '1rem 5vw 12rem',
+    display: "flex",
+    margin: "auto",
+    padding: "1rem 5vw 12rem",
     [mqMax[2]]: {
-      paddingTop: '5rem',
-      paddingBottom: '5rem'
+      paddingTop: "5rem",
+      paddingBottom: "5rem"
     },
     [mqMin[3]]: {
-      paddingBottom: '15rem'
+      paddingBottom: "15rem"
     },
     [mqMin[4]]: {
-      paddingBottom: '25rem'
+      paddingBottom: "25rem"
     },
     [mqMin[5]]: {
-      paddingBottom: '30rem'
+      paddingBottom: "30rem"
     },
-    minHeight: "100vh"    
+    minHeight: "100vh"
   }
 );
 
