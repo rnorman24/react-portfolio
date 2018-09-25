91d27e88e0b474c1e054dc9691079f011c411428
unknown <nray24@gmail.com>
2018-09-25 14:32:11 -0600

    refactor react

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -44,27 +44,3 @@ const Home = () => (
 );
 
 export default Home;
-
-/* {
-  <header
-  className="masthead text-center text-white"
-  style={{
-    backgroundImage: "url(" + imgUrl + ")",
-    backgroundSize: "cover",
-    overflow: "hidden"
-  }}
->
-  <h1 className="text-uppercase">
-    <strong>Ray Norman</strong>
-  </h1>
-  <img src={require("./images/rn-150-image.png")} alt="Raymond Norman" />
-  <hr />
-  <p className="text-faded mb-5">
-    Start Bootstrap can help you build better websites using the Bootstrap CSS
-    framework! Just download your template and start going, no strings
-    attached!
-  </p>
-  <a className="btn btn-primary btn-xl" href="#about">
-    Find Out More
-  </a>
-</header> */
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import styled from 'react-emotion';
 
+const About = () => (
+  <div></div>
+);
+
+export default About;
