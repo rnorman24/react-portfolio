6a4eb4a16f561ba8d33572d50bf06c4f7257603c
unknown <nray24@gmail.com>
2018-09-27 15:06:58 -0600

    update project list

--- src/pages/home/headerWrapper/DemoList.js
+++ src/pages/home/headerWrapper/DemoList.js
@@ -53,26 +53,10 @@ const DemoList = () => (
           bamazon MySQL Project
         </a>
       </DemoLi>
-      <DemoLi>
-        <a href="http://jensimmons.com/presentation/real-art-direction-web">
-          Revolutionize Your Page: Real Art Direction on the Web
-        </a>
-      </DemoLi>
-      <DemoLi>
-        <a href="http://jensimmons.com/presentation/progressing-our-layouts">
-          Progressing Our Layouts
-        </a>
-      </DemoLi>
-      <DemoLi>
-        <a href="http://jensimmons.com/presentation/modern-layouts-getting-out-our-ruts">
-          Modern Layouts: Getting Out of Our Ruts
-        </a>
-      </DemoLi>
-      <DemoLi>
-        <a href="http://jensimmons.com">jensimmons.com</a>
-      </DemoLi>
       <DemoLi style={BorderBottomStyle}>
-        <a href="../2017/workshop">Workshop Examples</a>
+        <a href="https://rnorman24.github.io/NYTSearch/">
+          New York Times Search Project
+        </a>
       </DemoLi>
     </DemoUl>
   </DemoStyle>
